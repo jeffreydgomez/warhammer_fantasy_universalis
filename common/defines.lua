337c5d5c7e820b1bbeec29ba5f8b27fc8b72753f
jeffreydgomez <jeffreydgomez@gmail.com>
2026-02-24 21:14:18 -0800

    Tweaks

--- common/defines.lua
+++ common/defines.lua
@@ -379,9 +379,9 @@ NDiplomacy = {
 	CELESTIAL_EMPIRE_MANDATE_PER_STATE_WITH_PROSPERITY = 0.04,	-- Yearly change of Mandate for each State with prosperity.
 	CELESTIAL_EMPIRE_MANDATE_PER_HUNDRED_DEVASTATION = -12.0,	-- Yearly change of Mandate for each hundred devastated development (scaled to devastation).
 	CELESTIAL_EMPIRE_MANDATE_PER_HUNDRED_TRIBUTARY_DEV = 0.15,	-- Yearly change of Mandate for each hundred development tributary state.
-	CELESTIAL_EMPIRE_MANDATE_PER_HUNDRED_NONTRIBUTARY_DEV = -0.15,	-- Yearly change of Mandate for each hundred development of neighbouring states that are not the Emperor's tributaries # WAR In vanilla this is 0, though in older versions it was higer.
+	CELESTIAL_EMPIRE_MANDATE_PER_HUNDRED_NONTRIBUTARY_DEV = -0.33,	-- Yearly change of Mandate for each hundred development of neighbouring states that are not the Emperor's tributaries # WAR In vanilla this is 0, though in older versions it was higer.
 	CELESTIAL_EMPIRE_MANDATE_FROM_DEFENDING = 5,	-- How much Mandate is gained when successfully defending the Emperor title.
-	CELESTIAL_EMPIRE_MANDATE_PER_5_LOANS = -1.00,	-- Yearly change of Mandate for every 5 loans.	# WAR this is normally -0.6
+	CELESTIAL_EMPIRE_MANDATE_PER_5_LOANS = -1.0,	-- Yearly change of Mandate for every 5 loans.	# WAR this is normally -0.6
 
 	REMOVE_ELECTORATE_INFLUENCE_COST = 10,			-- The amount of IA Remove Electorate costs.
 	GRANT_ELECTORATE_INFLUENCE = 0,					-- The amount of AI Grant Electorate gives.
